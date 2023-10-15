30947a693c26f5e18b7983589757f0b342b8cee4
Konrad Kadzielawa <kkadzi25@gmail.com>
2023-10-15 10:05:40 -0500

    allowed host - convert string to list;

--- llkanalytics/settings.py
+++ llkanalytics/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 # ALLOWED HOSTS
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
