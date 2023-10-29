af47b21ce21e897a0cb9fe892a6610b8b9eecdfb
Konrad Kadzielawa <kkadzi25@gmail.com>
2023-10-28 22:05:09 -0500

    django-bootstrap; contact form

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .forms import EmailPostForm
+from blog.models import Post
+
+def contact(request):
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+
+    return render(request, 'contact.html', {'form': form})
