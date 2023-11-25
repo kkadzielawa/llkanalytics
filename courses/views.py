9c9e7eb632a9d81e0cc03b0064d15117faae45a1
Konrad Kadzielawa <kkadzi25@gmail.com>
2023-11-25 12:45:02 -0600

    misc updates;

--- courses/views.py
+++ courses/views.py
@@ -4,36 +4,26 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category
 from .models import Course
 
+
 def course_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     courses = Course.objects.filter(available=True)
-    
+
     if category_slug:
-        category = get_object_or_404(Category, 
-                                    slug = category_slug,     
-                                    )
+        category = get_object_or_404(
+            Category,
+            slug=category_slug,
+        )
         courses = Course.objects.filter(category=category)
-    return render(request, 
-                'courses/list.html',
-                {
-                    'category': category,
-                    'categories': categories,
-                    'courses': courses
-                }           
+    return render(
+        request,
+        "courses/list.html",
+        {"category": category, "categories": categories, "courses": courses},
     )
 
-def course_detail(request, id, slug):
-    
-    course = get_object_or_404(Course, 
-                               id=id, 
-                               slug=slug,
-                               available=True
-                               )
-    
-    return render(request,
-                  'courses/detail.html',
-                  {
-                      'course': course
-                  }
-    )
+
+def course_detail(request, slug):
+    course = get_object_or_404(Course, slug=slug, available=True)
+
+    return render(request, "courses/detail.html", {"course": course})
