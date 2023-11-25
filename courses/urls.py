4c60cd254ae6c63e39bead691c4270e0fb021dfa
Konrad Kadzielawa <kkadzi25@gmail.com>
2023-11-25 11:12:55 -0600

    fix category error;

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'courses'
+app_name = "courses"
 
 urlpatterns = [
-        # post views
-        path('', views.course_list, name='course_list'),
-        path('<slug:slug>', views.course_list, name='course_list_by_category'),
-        path('<int:id>/<slug:slug>/', views.course_detail, name='course_detail'),
-    ]
+    # post views
+    path("", views.course_list, name="course_list"),
+    path("<slug:category_slug>", views.course_list, name="course_list_by_category"),
+    path("<int:id>/<slug:slug>/", views.course_detail, name="course_detail"),
+]
