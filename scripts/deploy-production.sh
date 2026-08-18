5cd4e5d11b90bf4d341547ab28b46c4e3b796b0b
Konrad Kadzielawa <kkadzi25@gmail.com>
2026-08-18 15:28:26 -0500

    updating sitemap; (#26)

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -22,5 +22,6 @@ git reset --hard "origin/$DEPLOY_BRANCH"
 
 docker compose -f docker-compose.prod.yml --env-file .env build
 docker compose -f docker-compose.prod.yml --env-file .env up -d
+docker compose -f docker-compose.prod.yml --env-file .env exec -T web python manage.py shell -c "from urllib.parse import urlparse; from django.conf import settings; from django.contrib.sites.models import Site; parsed = urlparse(settings.SITE_URL); domain = parsed.netloc or parsed.path; Site.objects.update_or_create(id=settings.SITE_ID, defaults={'domain': domain, 'name': 'LLKAnalytics'}); print(f'Synced django_site to {domain}')"
 docker compose -f docker-compose.prod.yml --env-file .env ps
 docker image prune -f
